4ba0c50b321c1d3959ded6c1dcef229c000487c8
LeoniSayat <l30.sayat@gmail.com>
2023-09-15 23:11:08 -0700

    Initial add

---
+++ Assets/flocking-algorithm-master/Behavior Scripts/StayInRadiusBehavior.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Flock/Behavior/Stay In Radius")]
+public class StayInRadiusBehavior : FlockBehavior
+{
+    public Vector2 center;
+    public float radius = 50f;
+
+    public override Vector2 CalculateMove(FlockAgent agent, List<Transform> context, Flock flock)
+    {
+        Vector2 centerOffset = center - new Vector2(agent.transform.position.x, agent.transform.position.z);
+        float t = centerOffset.magnitude / radius;
+        if (t < 0.9f)
+        {
+            return Vector2.zero;
+        }
+
+        return centerOffset * t * t;
+    }
+}
---
+++ Assets/flocking-algorithm-master/FlockAgent.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class FlockAgent : MonoBehaviour
+{
+
+    Flock agentFlock;
+    public Flock AgentFlock { get { return agentFlock; } }
+    public Vector3 _forward; 
+    public Vector3 _scuffed;
+    static Vector3 smooth = new Vector3(1,1,0);
+
+    Collider agentCollider;
+    public Collider AgentCollider { get { return agentCollider; } }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agentCollider = GetComponent<Collider>();
+    }
+
+    public void Initialize(Flock flock)
+    {
+        agentFlock = flock;
+    }
+
+    public void Move(Vector2 velocity)
+    {
+        //transform.forward = (Vector3)velocity;
+        _scuffed = ((Vector3)velocity).normalized;
+        //transform.forward = _scuffed;
+        //transform.forward = new Vector3(_scuffed.x, _scuffed.z, _scuffed.y);
+        //transform.forward = Vector3.Cross(((Vector3)velocity).normalized, smooth); 
+        // _scuffed = ((Vector3)velocity).normalized;
+        _forward = transform.forward;
+        transform.position += new Vector3(velocity.x, 0, velocity.y) * Time.deltaTime;
+
+    }
+}
